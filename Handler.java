a30215ecb4c4947b3ce47ac87ef919c6a3717292
CSF <48520093+QSQSQSQS@users.noreply.github.com>
2019-04-09 21:06:15 +0800

    improved handler
    an improved handler that let Mario move in a simple map but not successed

--- Handler.java
+++ Handler.java
@@ -1,16 +1,24 @@
 package com.tutorial.mario;
 
 import java.awt.Graphics;
+
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
+
 
 import com.tutorial.mario.entity.Entity;
 import com.tutorial.mario.entity.Player;
 import com.tutorial.mario.tile.Tile;
+import com.tutorial.mario.tile.Wall;
 
 public class Handler {
 
 	public LinkedList<Entity> entity = new LinkedList<Entity>();
 	public LinkedList<Tile> tile = new LinkedList<Tile>();
+	
+//	public Handler() {
+//		creatLevel(null);
+//	}
 	
 	public void render(Graphics g) {
 		for(Entity en: entity) {
@@ -19,7 +27,7 @@ public class Handler {
 		for(Tile ti: tile) {
 			ti.render(g);
 		}
-	}
+	}//
 	public void tick() {
 		for(Entity en: entity) {
 			en.tick();
@@ -29,20 +37,36 @@ public class Handler {
 		}
 	}
 	
-	public void addEntity(Entity en) {
-		entity.add(en);
+	public void addEntity(Entity e) {
+		entity.add(e);
 	}
 	
-	public void removeEntity(Entity en) {
-		entity.remove(en);
+	public void removeEntity(Entity e) {
+		entity.remove(e);
 	}
 	
-	public void addTile(Tile ti) {
-		tile.add(ti);
+	public void addTile(Tile t) {
+		tile.add(t);
 	}
 	
-	public void removeTile(Tile ti) {
-		tile.remove(ti);
+	public void removeTile(Tile t) {
+		tile.remove(t);
+	}
+	public void creatLevel(BufferedImage level) {
+		int width = level.getWidth();
+		int height = level.getHeight();
+		for(int y=0; y<height; y++) {
+			for (int x = 0; x < width; x++) {
+				int pixel = level.getRGB(x, y);
+				
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = (pixel) & 0xff;
+				
+				if(red==0 && green==0 && blue==0) addTile(new Wall(x*64, y*64, 64, 64, true, Id.wall, this));
+				if(red==0 && green==0 && blue==255) addEntity(new Player(x*64, y*64, 64, 64, false, Id.player, this));
+			}
+		}
 	}
 	
 }
